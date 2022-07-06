a8a486a515888d4ef80c9d6b7d402cbdf5826d61
Aidenkopec <Aidenkopec@icloud.com>
2022-07-06 17:16:05 -0600

    Commit/push

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import DeleteList from "./components/DeleteList";
+import DeleteList from "./components/Part1/DeleteList";
 import './partOneDelete.css'
 function App() {
   return (

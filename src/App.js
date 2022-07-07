13f20ae4fdf7cd40e0aa1f9162fa8f9c0e85a340
Aidenkopec <Aidenkopec@icloud.com>
2022-07-07 14:11:45 -0600

    Import SignUpform.js to App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import DeleteList from "./components/Part1/DeleteList";
 import './partOneDelete.css'
+import SignUpForm from "./components/Part2/SignUpForm";
+import './signUpForm.css'
+
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="App">
 
-      <DeleteList/>
-
-    </div>
-  );
+            <DeleteList/>
+            {/*<SignUpForm/>*/}
+        </div>
+    );
 }
 
 export default App;
